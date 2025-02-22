72e7b42af1ed58e49cd0cd4db6ab6e48bfc9d0a2
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-22 17:57:25 +0530

    install nginx ingress

--- helm_chart.sh
+++ helm_chart.sh
@@ -1,5 +1,5 @@
 #helm upgrade -i $1 -f values/$1.yaml ./chart
 #helm install $1 -f values/$1.yaml ./chart
-helm install frontend -f chart/values/frontend.yaml ./chart
+helm install $1 -f chart/values/$1.yaml ./chart
 
 
